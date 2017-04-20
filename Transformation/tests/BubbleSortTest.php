44d82a9b0b870cb2e534d531df9b78d6d1d4bd2f
mirko1593 <mirko1593@hotmail.com>
2017-04-20 20:25:35 +0800

    final test for bubble sort

--- Transformation/tests/BubbleSortTest.php
+++ Transformation/tests/BubbleSortTest.php
@@ -26,6 +26,21 @@ class BubbleSortTest extends PHPUnit\Framework\TestCase
         $this->assertSorted([1, 2, 3], [3, 2, 1]);
     }
 
+    /** @test */
+    public function integration_test_for_bubble_sort()
+    {
+        $elems = [];
+        foreach (range(1, 1000) as $i) {
+            $elems[] = random_int(1, 1000);
+        }
+
+        $sortedElems = $this->sorter->sort($elems);
+
+        for ($i = 0; $i < 999; $i++) {
+            $this->assertTrue($sortedElems[$i] <= $sortedElems[$i+1]);
+        }
+    }
+
     protected function assertSorted($expected, $input)
     {
         $this->assertSame($expected, $this->sorter->sort($input));
