9f2644898cc742bac1ea75b227e8b2b5feaa211a
mirko1593 <mirko1593@hotmail.com>
2017-04-20 20:41:38 +0800

    4th: add case; after 3rd transformation, this is the only option

--- Transformation/src/QuickSort.php
+++ Transformation/src/QuickSort.php
@@ -7,15 +7,26 @@ class QuickSort
     public function sort($elems)
     {
         $sortedElems = [];
-        if (sizeof($elems) <= 1) {
-            return $elems ?? [];
-        } 
-        if ($elems[0] > $elems[1]) {
-            $sortedElems[] = $elems[1];
+        if (sizeof($elems) == 1) {
             $sortedElems[] = $elems[0];
-        } else {
-            $sortedElems[] = $elems[0];
-            $sortedElems[] = $elems[1];
+        } else if (sizeof($elems) === 2) {
+            if ($elems[0] > $elems[1]) {
+                $sortedElems[] = $elems[1];
+                $sortedElems[] = $elems[0];
+            } else {
+                $sortedElems[] = $elems[0];
+                $sortedElems[] = $elems[1];
+            } 
+        } else if (sizeof($elems) === 3) {
+            if ($elems[1] > $elems[2]) {
+                $sortedElems[] = $elems[2];
+                $sortedElems[] = $elems[0];
+                $sortedElems[] = $elems[1];
+            } else {
+                $sortedElems[] = $elems[1];
+                $sortedElems[] = $elems[0];
+                $sortedElems[] = $elems[2];
+            }
         }
 
         return $sortedElems;
--- Transformation/tests/QuickSortTest.php
+++ Transformation/tests/QuickSortTest.php
@@ -22,6 +22,7 @@ class QuickSortTest extends PHPUnit\Framework\TestCase
     {
         $this->assertSorted([1], [1]);
         $this->assertSorted([1, 2], [2, 1]);
+        $this->assertSorted([1, 2, 3], [2, 1, 3]);
     }  
 
     protected function assertSorted($expected, $input)
