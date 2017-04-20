5797924d78874b23db555bfbefee2a35b1d0a15b
mirko1593 <mirko1593@hotmail.com>
2017-04-20 19:53:58 +0800

    first transformation: null to constant

---
+++ Transformation/src/BubbleSort.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Sort;
+
+class BubbleSort
+{
+    public function sort($elems)
+    {
+        return [];
+    }
+}
---
+++ Transformation/tests/BubbleSortTest.php
@@ -0,0 +1,29 @@
+<?php 
+
+use Sort\BubbleSort;
+
+class BubbleSortTest extends PHPUnit\Framework\TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->sorter = new BubbleSort;
+    }
+
+    /** @test */
+    public function nothing()
+    {
+        $this->assertSorted([], []);
+    }    
+
+    // /** @test */
+    // public function it_sorts_elems()
+    // {
+    //     $this->assertSorted([1], [1]);
+    // }
+
+    protected function assertSorted($expected, $input)
+    {
+        $this->assertSame($expected, $this->sorter->sort($input));
+    }
+}
