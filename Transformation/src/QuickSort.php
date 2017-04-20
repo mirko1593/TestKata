b661cb3a2cc7427c8d5c293f8294226a2286153b
mirko1593 <mirko1593@hotmail.com>
2017-04-20 20:35:38 +0800

    3rd: add computation trasformation instead of assign variable to elems

--- Transformation/src/QuickSort.php
+++ Transformation/src/QuickSort.php
@@ -6,6 +6,18 @@ class QuickSort
 {
     public function sort($elems)
     {
-        return $elems ?? [];
+        $sortedElems = [];
+        if (sizeof($elems) <= 1) {
+            return $elems ?? [];
+        } 
+        if ($elems[0] > $elems[1]) {
+            $sortedElems[] = $elems[1];
+            $sortedElems[] = $elems[0];
+        } else {
+            $sortedElems[] = $elems[0];
+            $sortedElems[] = $elems[1];
+        }
+
+        return $sortedElems;
     }
 }
--- Transformation/tests/QuickSortTest.php
+++ Transformation/tests/QuickSortTest.php
@@ -21,6 +21,7 @@ class QuickSortTest extends PHPUnit\Framework\TestCase
     public function it_quick_sorts_elems()
     {
         $this->assertSorted([1], [1]);
+        $this->assertSorted([1, 2], [2, 1]);
     }  
 
     protected function assertSorted($expected, $input)
